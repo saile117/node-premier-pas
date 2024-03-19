1959d3546a7cc76b58f0995ba1cfd7d499430b4a
ELIAS BEN BRAHIM <eliasben41@gmail.com>
2024-03-19 12:06:59 +0100

    blablou

---
+++ wilders.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Emilie'));
+
+console.log (chalk.red('Benoit'));
+
+console.log (chalk.yellow('Alexandre'));
+
+console.log (chalk.white('Vincent'));
